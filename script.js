f7d86672cce03fbb4f1f8be6c0da050941ca97db
Cindy-w44 <cindyw44@nycstudents.net>
2025-12-19 19:26:38 +0000

    finished

--- script.js
+++ script.js
@@ -44,9 +44,9 @@ console.log(sibling3.toUpperCase());
 // 9. Use console.log() and a built-in method to print out the string "ricardo montoya de la rosa ramirez"
 console.log(sibling3.toLowerCase());
 // 10. Use console.log() and a built-in method to print out Ricardo's name, but with all the instances of the letter a removed or deleted.
-
+console.log(sibling3.replaceAll("a", " "));
 // 11. Use console.log() and a built-in method to print out "De La Rosa"
-
+console.log(sibling3.substring(16, 26));
 // Medium Challenges
 
 // 12. Rewrite these function as an arrow function.
@@ -57,14 +57,14 @@ function getRandomNum() {
 }
 
 const getRandomNum2 = () => (Math.floor(Math.random() * 100));
-console.log(getRandomNum2);
+console.log(getRandomNum2());
 
 function greeting(name) {
     return `Hello ${name}, I'm glad you can make it!`;
 }
 
 const greeting2 = (name) => (`Hello ${name}, I'm glad you can make it!`);
-console.log(greeting2);
+console.log(greeting2("Teacher"));
 
 function perfectRoot(x) {
     let root = Math.sqrt(x);
@@ -75,16 +75,21 @@ function perfectRoot(x) {
     }
 }
 
-const perfectRoot2 = (x) => 
+const perfectRoot2 = (x) => {
+    let root = Math.sqrt(x);
+    return Number.isInteger(root) ? root : "Not a root";
+}
+console.log(perfectRoot2(36));
+console.log(perfectRoot2(24));
 
 // 13. Fix/Finish the following functions.
 /// Then, call them with at least 2 different inputs to ensure they work.
 
 /// a. Adds 2 numbers and returns the sum
-// const addNums = (x, y) => {
-//     let sum = x + y;
-//     return sum;
-// };
+const addNums = (x, y) => {
+    let sum = x + y;
+    return sum;
+};
 // uncomment the line below, to call the function, and add one more function call
 console.log(addNums(4, 8));
 
@@ -117,10 +122,14 @@ console.log(reduceEvens(21));
 // 14. Write a function called shortenString(string) that returns the first 10 characters of a string if it is longer than 10
 //remove all
 const shortenString = (string) => {
-
+    if (string.length < 10) {
+        return string;
+    } else {
+        return string.slice(0, 10);
+    }
 }
-
-
+console.log(shortenString("This is a very long string"));
+console.log(shortenString("Shorty"));
 
 // Spicy Challenges
 let burger = 5.99;
@@ -131,20 +140,32 @@ let soda = 1.99;
 /// If no burgers can be bought, return "Sorry, no burgers for you."
 
 const buyBurgers = (budget) => {
-
-}
+    if (budget > burger) {
+        let burgers = Math.floor(budget / burger);
+        return "You can buy " + burgers + " burgers";
+    }
+    return "Sorry, no burgers for you";
+};
+console.log(buyBurgers(20));
 
 // 16. A meal consists of 1 burger, 1 fry, and 1 soda.
 /// Write a function called buyMeals(budget) that tells the user how many meals they can buy and how much money they will have left over.
 /// Bonus: Round the answer to the nearest penny.
+let happyMeal = burger + fries + soda;
 const buyMeals = (budget) => {
-
+    if (budget > happyMeal) {
+        return `You have ${budget - happyMeal} left!`;
+    }
+    return "No money for Happy Meal..."
 }
-
+console.log(buyMeals(50));
 
 // 17. [Challenge] Write a function missingLeg(side1, side2) that takes in the length of a hypotenuse and a leg of a right triangle and prints out the missing leg's length
 /// The larger side should be the hypotenuse  
-
+const missingLeg = (side1, side2) => {
+    return Math.sqrt((side1 * side1) + (side2 * side2));
+}
+console.log(missingLeg(5, 4));
 
 
 // 18. [Super Challenge] In math, Factorial is the product of an integer and all the integers below it.
